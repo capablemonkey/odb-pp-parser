9da625d71e0a48ce3995d6fc698c894e321df08d
Gordon Zheng <gordon@greenhouse.io>
2016-12-30 18:10:14 -0500

    Serialize numbers as JSON numbers instead of strings

--- lib/parseable.rb
+++ lib/parseable.rb
@@ -1,5 +1,6 @@
 class Parseable
   REGEX = nil
+  NUMBER_FIELDS = ['width', 'height', 'x', 'y', 'diameter']
 
   def self.parse(file_lines)
     raise "No regex defined" if regex.nil?
@@ -19,6 +20,17 @@ class Parseable
     end
 
     def self.match_data_to_hash(match_data)
-      match_data.names.zip(match_data.captures).to_h
+      hash = create_hash(match_data.names, match_data.captures)
+      parse_number_fields(hash)
+    end
+
+    def self.parse_number_fields(match_hash)
+      match_hash.map do |key, value|
+        [key, NUMBER_FIELDS.include?(key) ? Float(value) : value]
+      end.to_h
+    end
+
+    def self.create_hash(keys, values)
+      keys.zip(values).to_h
     end
 end
