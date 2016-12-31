711c752dd29ebaf59e6c1deede8fb9eb43f90834
Gordon Zheng <gordon@greenhouse.io>
2016-12-30 21:51:53 -0500

    Round off coordinate values when matching points

--- lib/parsers/netlist_parser.rb
+++ lib/parsers/netlist_parser.rb
@@ -1,6 +1,9 @@
 class NetlistParser
   attr_accessor :nets, :netpoints
 
+  # decimal places to match:
+  POINT_MATCH_PRECISION = 5
+
   def initialize(file_lines)
     @nets = OdbNet::parse_nets(file_lines)
     @netpoints = NetPoint::parse_netpoints(file_lines)
@@ -18,7 +21,13 @@ class NetlistParser
   end
 
   def get_net_at_point(x, y)
-    @points_to_net ||= @netpoints.map { |point| ["#{point.x},#{point.y}", point.net] }.to_h
-    @points_to_net["#{x},#{y}"]
+    @points_to_net ||= @netpoints.map { |point| [point_key(point.x, point.y), point.net] }.to_h
+    @points_to_net[point_key(x, y)]
   end
+
+  private
+
+    def point_key(x, y)
+      "#{x.round(POINT_MATCH_PRECISION)},#{y.round(POINT_MATCH_PRECISION)}"
+    end
 end
